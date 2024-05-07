b1937f3487eafaf8a00f73ef64f71f428c6b1ce9
peregomon <160238970+peregomon@users.noreply.github.com>
2024-05-07 12:34:25 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,38 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCage(bool light)
+{
+	if (!first) {
+        Cage* newCage = new Cage;
+        newCage->light = light;
+        newCage->next = newCage;
+        newCage->prev = newCage;
+        first = newCage;
+    }
+    else {
+        
+        Cage* newCage = new Cage;
+        newCage->light = light;
+        newCage->next = first;
+        newCage->prev = first->prev;
+        first->prev->next = newCage;
+        first->prev = newCage;
+    }
+}
+
+int Train::getLength()
+{
+    first->light = true;
+    Cage* tmp;
+    while (first->light == true) {
+        countOp = 1;
+        tmp = first;
+        while (tmp->next->light == false) {
+            countOp++;
+            tmp = tmp->next;
+        }
+        tmp->next->light = 0;
+    }
+    return countOp;
+}
