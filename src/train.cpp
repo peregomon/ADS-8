dc996e86f7cdb47920c3c820af51ffbdd5bde5b3
peregomon <160238970+peregomon@users.noreply.github.com>
2024-05-07 20:33:09 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,7 +3,7 @@
 
 void Train::addCage(bool light)
 {
-	if (!first) {
+    if (!first) {
         Cage* newCage = new Cage;
         newCage->light = light;
         newCage->next = newCage;
@@ -24,15 +24,32 @@ void Train::addCage(bool light)
 int Train::getLength()
 {
     first->light = true;
-    Cage* tmp;
-    while (first->light == true) {
-        countOp = 1;
-        tmp = first;
-        while (tmp->next->light == false) {
-            countOp++;
+    Cage* tmp = first;
+    int cnt = 0;
+    int step = 0;
+    while (true) {
+        cnt = 1;
+        countOp += 1;
+        step += 1;
+        tmp = tmp->next;
+        while (tmp->light == false) {
+            step += 1;
+            countOp += 1;
+            cnt += 1;
             tmp = tmp->next;
         }
-        tmp->next->light = 0;
+        tmp->light = 0;
+        
+        tmp = tmp->prev;
+        countOp += 1;
+        step--;
+        while (step > 0) {
+            countOp += 1;
+            step -= 1;
+            tmp = tmp->prev;
+        }
+        if (tmp->light == false) return cnt;
     }
-    return countOp;
 }
+
+
